b49cac27140bba9fb262780af0d93d10b0deef9d
Iswanson81459 <Iswanson81459@uat.edu>
2025-10-05 21:32:48 -0700

    First Push for proper repository for Asteriods
    Adding all files to new repository

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Pawn pawn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Based on inputs, send commands to pawn
+        MakeDecistions();
+        
+    }
+
+    private void MakeDecistions() 
+    {
+
+        // Basic Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Tell Pawn to Move Forward
+            pawn.MoveForward(pawn.moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            // Tell Pawn to move backward
+            pawn.MoveBackword(pawn.moveSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            pawn.RoatateClockwise(pawn.turnSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pawn.RoatateCounterClockwise(pawn.turnSpeed);
+        }
+
+
+        // Tele Movement
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pawn.TeleportForward(pawn.teleDistance);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pawn.TeleportBackward(pawn.teleDistance);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            pawn.TeleportLeft(pawn.teleDistance);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            pawn.TeleportRight(pawn.teleDistance);
+        }
+
+        //Shift Boost
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            pawn.BoostSpeed(pawn.boostSpeed);
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            pawn.UnBoostSpeed(pawn.boostSpeed);
+        }
+    }
+}
---
+++ Assets/Script/Pawn.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class Pawn : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
+    public float teleDistance;
+
+    public float teleDistMin;
+    public float teleDistMax;
+
+    public float boostSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveForward(float moveSpeed)
+    {
+        // Change my pawn position -- in the forward direction, magnitude of moveSpeed
+        // get the transform component
+        transform.position = transform.position + ((transform.up * moveSpeed) * Time.deltaTime );
+
+    }
+    public void MoveBackword(float moveSpeed) 
+    {
+        transform.position = transform.position + ((-transform.up * moveSpeed) * Time.deltaTime);
+    }
+
+    public void RoatateClockwise ( float rotateValue) 
+    {
+        transform.Rotate(0, 0f, -rotateValue * Time.deltaTime);
+    }
+
+    public void RoatateCounterClockwise(float rotateValue)
+    {
+        transform.Rotate(0, 0f, rotateValue * Time.deltaTime);
+    }
+
+    public void TeleportForward(float TeleDistance)
+    {
+        float moveDist = Random.Range(teleDistMin, teleDistMax);
+
+        transform.Translate(Vector3.up * moveDist, Space.World);
+    }
+    public void TeleportBackward(float TeleDistance)
+    {
+        float moveDist = Random.Range(teleDistMin, teleDistMax);
+
+        transform.Translate(Vector3.down * moveDist, Space.World);
+    }
+    public void TeleportLeft(float TeleDistance)
+    {
+        float moveDist = Random.Range(teleDistMin, teleDistMax);
+
+        transform.Translate(Vector3.left * moveDist, Space.World);
+    }
+    public void TeleportRight(float TeleDistance)
+    {
+        float moveDist = Random.Range(teleDistMin, teleDistMax);
+
+        transform.Translate(Vector3.right * moveDist, Space.World);
+    }
+
+    public void BoostSpeed(float boostSpeed)
+    {
+        this.moveSpeed += boostSpeed;
+    }
+
+    public void UnBoostSpeed(float boostSpeed)
+    {
+        this.moveSpeed -= boostSpeed;
+    }
+}
