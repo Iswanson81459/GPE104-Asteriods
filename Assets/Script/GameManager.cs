bf0a86aa09d4508eb230c225c83d4f76e66e01e9
Iswanson81459 <Iswanson81459@uat.edu>
2025-10-21 15:22:43 -0700

    First push of UI Canvas
    UI and Canvas

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,62 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // there can only be one esconor the one game manager 
+    public static GameManager instance;
+    public List<DamageOnOverlap> damageZones;
+    public Pawn player;
+
+
+    // So that the game result win/lose only plays onces
+    private bool playGameResult = true;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+
+        // Start w/ 0 damage zones
+        damageZones = new List<DamageOnOverlap>();
+        
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damageZones.Count <= 0 && playGameResult)
+        { 
+            WinGame();
+        }
+        else if (player == null && playGameResult)
+        {
+            LoseGame();
+        }
+        
+    }
+
+    void WinGame()
+    {
+        Debug.Log("Great work Soldier!!!!");
+        playGameResult = false; 
+    }
+
+    void LoseGame()
+    {
+        Debug.Log("Disapointing Failure!!!!");
+        playGameResult = false;
+    }
+}
---
+++ Assets/Script/Health.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float currentHealth;
+    public float maxHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth = currentHealth - amount;
+
+        if (!isAlive())
+        {
+            // Change the amount of damage based on armor value
+
+            currentHealth = 0;
+            Die();
+        }
+    }
+
+    public void Heal (float amount)
+    {
+        currentHealth = currentHealth + amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public void Die()
+    {
+        // TODO: handle Deah in the health componet
+        Death death = GetComponent<Death>();
+        // if death Component exists
+        if (death != null)
+        {
+            death.Die();
+        }
+    }
+
+    public bool isAlive()
+    {
+        if(currentHealth > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
